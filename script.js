45dd6f97f65836fb8334dbb6db8088334c4bf3da
ANDERSON VIDAL  DE SOUZA MATOS MIRANDA <F4009@icc.local>
2022-10-03 15:05:37 -0300

    calculadora imc

---
+++ script.js
@@ -0,0 +1,23 @@
+function calculate(){
+    var height = (document.getElementById ("height").value/100)
+    var weight=document.getElementById ("weight").value;
+    
+    var imc = weight / (height) ** 2;
+    
+    if(imc<18.5) {
+        text="Você está magro!"
+    }
+    else if(imc<24.9){
+        text="Você está normal!"
+    }
+    else if(imc<29.9){
+        text="Você está com sobrepeso!"
+    }
+    else if(imc<39.9){
+        text="Você está obesidade!"
+    }
+    else if(imc>39.9){
+        text="Você está obesidade morbida!"
+    }
+    document.getElementById("text_area").innerText=text
+}
